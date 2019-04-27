c76c6719aaac61e63c4cda730a260eb807b2b2c0
CristiAllan <cristiallan@gmail.com>
2019-04-27 12:14:26 -0300

    generating scaffold to ProfilePage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-import Header from './components/Header/Header';
+import  Header from './components/Header/Header';
+import ProfilePageContainer from './containers/ProfilePageContainer/ProfilePageContainer';
+import { Container } from 'react-materialize';
 
 class App extends Component {
   render() {
     return (
-      <Header />
+      <div>
+        <Header />
+        <Container>
+          <ProfilePageContainer/>
+        </Container>
+      </div>
     );
   }
 }
---
+++ src/containers/ProfilePageContainer/ProfilePageContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ProfilePage from '../../components/ProfilePage/ProfilePage';
+
+class ProfilePageContainer extends Component {
+  render() {
+    return (
+      <ProfilePage />
+    );
+  }
+}
+
+export default ProfilePageContainer;
