446228a6b28495decc96d55002a2d342512cd50d
CristiAllan <cristiallan@gmail.com>
2019-04-27 15:03:54 -0300

    generate a scaffold to the project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import  Header from './components/Header/Header';
 import ProfilePageContainer from './containers/ProfilePageContainer/ProfilePageContainer';
-import { Container } from 'react-materialize';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <Container>
-          <ProfilePageContainer/>
-        </Container>
-      </div>
+      <ProfilePageContainer/>
     );
   }
 }
--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-materialize';
+import MainLayout from '../MainLayout';
 
 const ProfilePage = () => (
-  <Row>
-    <Col s={12} m={3}>
-      User Info
-    </Col>
+  <MainLayout>
+    <Row>
+      <Col s={12} m={3}>
+        User Info
+      </Col>
 
-    <Col s={12} m={6}>
-      Tweet List
-    </Col>
+      <Col s={12} m={6}>
+        Tweet List
+      </Col>
 
-    <Col s={12} m={3}>
-      Trending Topics
-    </Col>
-  </Row>
+      <Col s={12} m={3}>
+        Trending Topics
+      </Col>
+    </Row>
+  </MainLayout>
 );
 
-export default ProfilePage
+export default ProfilePage;
